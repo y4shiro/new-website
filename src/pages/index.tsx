9b2d34184e01120c8c22fffc2ac6dfee99d9dc1b
y4shiro <y4shironao@gmail.com>
2021-07-07 19:18:05 +0900

    Emotion css Props 用の設定追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
-import * as React from "react"
+import React from 'react';
+import { css } from '@emotion/react';
 
 const IndexPage: React.FC = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>
-        Web Page
-      </h1>
+      <h1 css={h1}>Web Page</h1>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+const h1 = css`
+  background-color: gray;
+`;
+
+export default IndexPage;
