d96a13c29925a8ac1512eb8976e4677550357929
y4shiro <y4shironao@gmail.com>
2021-07-07 19:09:35 +0900

    TypeScript 導入

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage: React.FC = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>
+        Web Page
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
