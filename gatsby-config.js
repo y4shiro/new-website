15c90cd6470c6f551c1fcb9568158861af8d23ad
y4shiro <y4shironao@gmail.com>
2021-08-22 14:31:32 +0900

    gatsby-source-filesystem インストール

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,19 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ],
 };
