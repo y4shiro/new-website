54f153f1abddbe7f66db30711a9dcc37622b75ec
y4shiro <y4shironao@gmail.com>
2021-08-23 19:00:35 +0900

    Skill にカードスタイルを設定

--- src/components/Skills/SkillList.tsx
+++ src/components/Skills/SkillList.tsx
@@ -10,19 +10,58 @@ type skillProps = {
 
 const NewSkill: React.FC<skillProps> = ({ skillList }) => {
   return (
-    <ul>
+    <ul css={ulStyles}>
       {skillList.map((skill) => {
         const src = `/skillIcons/${skill.iconPath}.svg`;
 
         return (
-          <li key={skill.name}>
-            <p>{skill.name}</p>
-            <img src={src} width="32px" alt="" />
+          <li css={liStyles} key={skill.name}>
+            <div>
+              <p>{skill.name}</p>
+              <img src={src} alt="" />
+            </div>
           </li>
         );
       })}
     </ul>
   );
 };
+
+const ulStyles = css`
+  padding: 0;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+const liStyles = css`
+  width: 30%;
+  padding: 4px;
+  background-color: #fafafa;
+  list-style: none;
+  text-align: center;
+  border-radius: 4px;
+  box-shadow: 2px 2px 5px 4px #ddd;
+  transition: 0.2s ease-in-out;
+
+  div {
+    padding: 0 16px 16px 16px;
+
+    p {
+      font-size: 1.125rem;
+    }
+    img {
+      max-width: 4rem;
+      margin: 16px;
+    }
+  }
+
+  &:hover {
+    box-shadow: 5px 5px 5px 4px #ccc;
+    transform: translateY(-2%);
+  }
+`;
 
 export default NewSkill;
