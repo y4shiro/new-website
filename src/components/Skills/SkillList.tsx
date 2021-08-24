8e88549b61feef199131b1c4b153973e74b2f288
y4shiro <y4shironao@gmail.com>
2021-08-24 22:39:32 +0900

    不要な css props を削除

--- src/components/Skills/SkillList.tsx
+++ src/components/Skills/SkillList.tsx
@@ -60,32 +60,4 @@ const ulStyles = css`
   gap: 1rem;
 `;
 
-const liStyles = css`
-  width: 46%;
-  padding: 4px;
-  background-color: #fafafa;
-  list-style: none;
-  text-align: center;
-  border-radius: 4px;
-  box-shadow: 2px 2px 5px 4px #ddd;
-  transition: 0.2s ease-in-out;
-
-  div {
-    padding: 0 16px 16px 16px;
-
-    p {
-      font-size: 1.125rem;
-    }
-    img {
-      max-width: 4rem;
-      margin: 16px;
-    }
-  }
-
-  &:hover {
-    box-shadow: 5px 5px 5px 4px #ccc;
-    transform: translateY(-2%);
-  }
-`;
-
 export default SkillList;
