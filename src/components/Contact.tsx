6a9a74add0ce75bb9d0b8ec14bfb23266b4a8c97
y4shiro <y4shironao@gmail.com>
2021-09-02 17:37:24 +0900

    各連絡先を a タグでリンク化

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { sectionTitle } from '../styles/styles';
+import { ReactSVG } from 'react-svg';
 
 const Contact: React.FC = () => {
   return (
     <section>
       <h2 css={sectionTitle}>Contact</h2>
       <ul>
-        <li>E-mail: y4shironao@gmail.com</li>
-        <li>GitHub: https://github.com/y4shiro/</li>
-        <li>はてなブログ: https://ram.hatenablog.jp/</li>
+        <li>
+          <a href="mailto:y4shironao@gmail.com" target="_blank">
+            E-mail: y4shironao@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/y4shiro/" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://ram.hatenablog.jp/" target="_blank">
+            はてなブログ
+          </a>
+        </li>
       </ul>
     </section>
   );
