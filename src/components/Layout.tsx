19b0a8761bb7790ab12ec7d3e9642dd4ebffb957
y4shiro <y4shironao@gmail.com>
2021-09-03 18:19:49 +0900

    font-family から Arial を削除した

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,8 +19,8 @@ const Layout: React.FC = ({ children }) => {
 const globalStyles = css`
   body {
     background-color: #e6e6e6;
-    font-family: 'Helvetica Neue', Arial, 'Hiragino Kaku Gothic ProN',
-      'Hiragino Sans', Meiryo, sans-serif;
+    font-family: 'Helvetica Neue', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans',
+      Meiryo, sans-serif;
   }
 `;
 
