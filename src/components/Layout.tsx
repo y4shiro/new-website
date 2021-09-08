202dccea76094fa90667752b0810b31a72cb3cce
y4shiro <y4shironao@gmail.com>
2021-09-08 11:01:33 +0900

    SEO.tsx をページ毎に読み込むよう変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { Global, css } from '@emotion/react';
 import emotionNormalize from 'emotion-normalize';
 
-import SEO from './SEO';
-
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <SEO />
       <Global
         styles={css`
           ${emotionNormalize}
