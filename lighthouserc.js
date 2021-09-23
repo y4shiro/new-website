e381fd0bebd369d1f13044b9cdb62884b25e4f26
y4shiro <y4shironao@gmail.com>
2021-09-23 21:42:47 +0900

    Heroku に Lighthouse CI Server をセットアップ

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,9 @@ module.exports = {
       url: ['/'],
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://rocky-caverns-87987.herokuapp.com',
+      token: '12a30950-2c0c-475f-98aa-519cf2266258',
     },
   },
 };
